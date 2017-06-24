b8c59da723708e5d9cd463a79af38141707ac7ee
Joachim Chapman <joachim@the.quack.zone>
2017-06-24 09:59:36 +0100

    improve reliability by enabling ad blocker include rubicon jeeps in search

--- UteSearch/Program.fs
+++ UteSearch/Program.fs
@@ -2,9 +2,21 @@
 open Chiron
 
 canopy.configuration.chromeDir <- @"."
+// let adBlockExtensionPath = @"C:\Users\joachim\AppData\Local\Google\Chrome\User Data\Profile 1\Extensions\gighmmpiobklfepjocnamgkkbiglidom"
+let adBlockExtensionPath = @"C:\Users\joachim\AppData\Local\Google\Chrome\User Data\Profile 1\Extensions\gighmmpiobklfepjocnamgkkbiglidom\3.12.0_0"
+
+let chromeOptions = OpenQA.Selenium.Chrome.ChromeOptions()
+chromeOptions.AddArgument("load-extension=" + adBlockExtensionPath)
+// ChromeOptions options = new ChromeOptions();
+// options.addArguments("load-extension=/path/to/extension");
+// DesiredCapabilities capabilities = new DesiredCapabilities();
+// capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+// ChromeDriver driver = new ChromeDriver(capabilities);
 
 let (searches : Scraper.Search array) = 
     [|
+        {name = "rubicon 2dr"; startPageUrl = "http://www.carpoint.com.au/all-cars/results.aspx?q=((Service%3d%5bCarsales%5d%26((SiloType%3d%5bDealer+used+cars%5d%7cSiloType%3d%5bDemo+and+near+new+cars%5d)%7cSiloType%3d%5bPrivate+seller+cars%5d))%26(Make%3d%5bJeep%5d%26(Model%3d%5bWrangler%5d%26Badge%3d%5bRubicon%5d)))&sortby=TopDeal"}
+        {name = "rubicon 4dr"; startPageUrl = "http://www.carpoint.com.au/all-cars/results.aspx?q=((Service%3d%5bCarsales%5d%26((SiloType%3d%5bDealer+used+cars%5d%7cSiloType%3d%5bDemo+and+near+new+cars%5d)%7cSiloType%3d%5bPrivate+seller+cars%5d))%26(Make%3d%5bJeep%5d%26(Model%3d%5bWrangler%5d%26Badge%3d%5bUnlimited+Rubicon%5d)))&sortby=TopDeal"}
         {name = "Amarok 4x4 4cyl"; startPageUrl = "http://www.carpoint.com.au/all-cars/results.aspx?q=(((((Service%3d%5bCarsales%5d%26(Make%3d%5bVolkswagen%5d%26Model%3d%5bAmarok%5d))%26State%3d%5bSouth+Australia%5d)%26Drive%3d%5b4x4%5d)%26((SiloType%3d%5bDealer+used+cars%5d%7cSiloType%3d%5bDemo+and+near+new+cars%5d)%7cSiloType%3d%5bPrivate+seller+cars%5d))%26Cylinders%3d%5b4%5d)&sortby=TopDeal"}
         {name = "hilux 4x4 4cyl"; startPageUrl = "http://www.carpoint.com.au/all-cars/results.aspx?q=(((((((Service%3d%5bCarsales%5d%26((SiloType%3d%5bDealer+used+cars%5d%7cSiloType%3d%5bDemo+and+near+new+cars%5d)%7cSiloType%3d%5bPrivate+seller+cars%5d))%26(Make%3d%5bToyota%5d%26Model%3d%5bHilux%5d))%26State%3d%5bSouth+Australia%5d)%26BodyStyle%3d%5bUte%5d)%26Drive%3d%5b4x4%5d)%26Doors%3d%5b4%5d)%26Cylinders%3d%5b4%5d)&sortby=TopDeal"}
         {name = "ford ranger 2.2L"; startPageUrl = "http://www.carpoint.com.au/all-cars/results.aspx?q=((((((((Service%3d%5bCarsales%5d%26((SiloType%3d%5bDealer+used+cars%5d%7cSiloType%3d%5bDemo+and+near+new+cars%5d)%7cSiloType%3d%5bPrivate+seller+cars%5d))%26(Make%3d%5bFord%5d%26Model%3d%5bRanger%5d))%26State%3d%5bSouth+Australia%5d)%26BodyStyle%3d%5bUte%5d)%26Drive%3d%5b4x4%5d)%26Doors%3d%5b4%5d)%26Cylinders%3d%5b4%5d))%26(EngineSize%3drange%5b..2500%5d)&sortby=TopDeal"}
@@ -23,7 +35,8 @@ let writeResult dataDir (result :Scraper.Model.Result) =
     | None -> ()
 
 let search onResult search = 
-    start chrome
+    // start chrome
+    start <| ChromeWithOptions chromeOptions
 
     try
         Scraper.parseSearchPage onResult search
